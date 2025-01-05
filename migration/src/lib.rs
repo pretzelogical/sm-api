0d63e3a8ef6995745d7305b59b5a82722e7d8775
pretzelogical <yingyangyo266@gmail.com>
2025-01-05 17:31:18 -0600

    seed post

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20220101_000001_create_table;
 mod m20250105_080123_seed_user;
 mod m20250105_223608_create_post_table;
+mod m20250105_232528_seed_post;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20250105_080123_seed_user::Migration),
             Box::new(m20250105_223608_create_post_table::Migration),
+            Box::new(m20250105_232528_seed_post::Migration),
         ]
     }
 }
--- migration/src/m20250105_232528_seed_post.rs
+++ migration/src/m20250105_232528_seed_post.rs
@@ -1,5 +1,5 @@
-use sea_orm::{Set, ActiveModelTrait, ModelTrait, EntityTrait};
 use sea_orm_migration::prelude::*;
+use sea_orm::{Set, ActiveModelTrait, ModelTrait, EntityTrait};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -9,10 +9,11 @@ impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let db = manager.get_connection();
 
-        sm_entity::user::ActiveModel {
+        sm_entity::post::ActiveModel {
             id: Set(1),
-            name: Set("Test".to_owned()),
-            pass: Set("pass".to_owned()),
+            author_id: Set(1),
+            title: Set("Test post".to_owned()),
+            content: Set("Test post content :3:3:3".to_owned())
         }
         .insert(db)
         .await
@@ -24,13 +25,13 @@ impl MigrationTrait for Migration {
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let db = manager.get_connection();
 
-        let user = sm_entity::user::Entity::find_by_id(1)
+        let post = sm_entity::post::Entity::find_by_id(1)
             .one(db)
             .await?;
-        if let Some(user) = user {
-            user.delete(db).await?;
+        if let Some(post) = post {
+            post.delete(db).await?;
         } else {
-            panic!("Cannot find seeded user");
+            panic!("Cannot find seeded post");
         }
         Ok(())
     }
