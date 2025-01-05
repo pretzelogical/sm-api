892af6d7051982e9c513f10d69ebd94dfa4f902a
pretzelogical <yingyangyo266@gmail.com>
2025-01-05 16:35:30 -0600

    add post model

--- entity/src/post.rs
+++ entity/src/post.rs
@@ -3,25 +3,29 @@ use sea_orm::entity::prelude::*;
 
 
 #[derive(Serialize, Deserialize, Debug, Clone, DeriveEntityModel)]
-#[sea_orm(table_name = "sm_user")]
+#[sea_orm(table_name = "post")]
 pub struct Model {
     #[sea_orm(primary_key)]
     #[serde(skip_deserializing)]
     pub id: i64,
-    pub name: String,
-    pub pass: String,
+    pub author_id: i64,
+    pub title: String,
+    pub content: String,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
-    #[sea_orm(has_many = "super::post::Entity")]
-    Post
+    #[sea_orm(
+        belongs_to = "super::user::Entity",
+        from = "Column::AuthorId",
+        to = "super::user::Column::Id"
+    )]
+    User
 }
 
-
-impl Related<super::post::Entity> for Entity {
+impl Related<super::user::Entity> for Entity {
     fn to() -> RelationDef {
-        Relation::Post.def()
+        Relation::User.def()
     }
 }
 
