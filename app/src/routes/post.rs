92cb3115f67c6efd320038469fcb50f08f34de1d
pretzelogical <yingyangyo266@gmail.com>
2025-01-06 05:27:32 -0600

    add comment to already existing post

--- app/src/routes/post.rs
+++ app/src/routes/post.rs
@@ -38,20 +38,6 @@ async fn _get_comments(
         }
         Err(error) => Err(AppError::DbError(AppDbError::from(error))),
     }
-    // match post
-    //     .find_related(sm_entity::comment::Entity)
-    //     .all(db_client)
-    //     .await
-    // {
-    //     Ok(comments) => {
-    //         if comments.len() > 0 {
-    //             Ok(Some(comments))
-    //         } else {
-    //             Ok(None)
-    //         }
-    //     }
-    //     Err(error) => Err(AppError::DbError(AppDbError::from(error))),
-    // }
 }
 
 async fn get_by_id(
@@ -211,5 +197,42 @@ pub async fn get_comments(
         }
     } else {
         AppError::NotFound("comment not found").into()
+    }
+}
+
+
+#[derive(Deserialize)]
+pub struct CreateCommentForm {
+    pub author_id: i64,
+    pub title: String,
+    pub content: String
+}
+
+#[derive(Deserialize)]
+pub struct CreateCommentArgs {
+    pub comment: CreateCommentForm
+}
+
+#[post("/post/{id}/comment")]
+pub async fn create_comment(
+    app_state: web::Data<AppState>,
+    post_id: web::Path<i64>,
+    args: web::Json<CreateCommentArgs>
+) -> impl Responder {
+    use ActiveValue::Set;
+    let comment_form = &args.comment;
+    let comment = sm_entity::comment::ActiveModel {
+        post_id: Set(post_id.into_inner()),
+        author_id: Set(comment_form.author_id),
+        title: Set(comment_form.title.clone()),
+        content: Set(comment_form.content.clone()),
+        ..Default::default()
+    }
+        .insert(&app_state.db_client)
+        .await;
+
+    match comment {
+        Ok(comment) => HttpResponse::Ok().json(comment),
+        Err(error) => AppError::DbError(AppDbError::from(error)).into()
     }
 }
